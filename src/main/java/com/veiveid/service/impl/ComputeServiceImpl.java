561d94d1288a936fa6d306111799397a9c0be621
weizhang240 <weizhang240@creditease.cn>
2018-02-08 18:56:58 +0800

    init

---
+++ src/main/java/com/veiveid/service/impl/ComputeServiceImpl.java
@@ -0,0 +1,15 @@
+package com.veiveid.service.impl;
+
+import com.veiveid.service.ComputeService;
+
+/**
+ * Created by zhaiyc on 2016/7/14.
+ */
+public class ComputeServiceImpl implements ComputeService {
+
+    @Override
+    public Integer add(int a, int b) {
+        return a + b;
+    }
+
+}
